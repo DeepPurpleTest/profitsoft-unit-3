f9b836b15dc360acaa1c93a071f9ded5c643ab82
Moksem <maks.viskovatov@gmail.com>
2024-06-25 13:43:30 +0300

    add envs

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ const config = {
   // Services
   USERS_SERVICE: 'http://localhost:3000',
 
-  PROJECTS_SERVICE: 'http://localhost:1000',
+  PROJECTS_SERVICE: `${process.env.REACT_APP_BACKEND_API}` || '',
 
   UI_URL_PREFIX: process.env.REACT_APP_UI_URL_PREFIX || '',
 };
